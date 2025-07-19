0471e8cd41325b1f83b4144458d9685eb78206a3
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-07-19 09:15:48 +0530

    FEAT added playRound and playGame functions

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
  
 
+
 function getComputerChoice() {
 
     // create variable with random number
@@ -31,5 +32,85 @@ function getHumanChoice(){
     return userInput;
 }
 
-console.log("Computer Choice - ", getComputerChoice());
-console.log("Human Choice - ", getHumanChoice());
+
+
+//play game
+function playGame(){
+    //variable to keep track of human score
+    let humanScore = 0;
+
+    //variable to keep track of computer score
+    let computerScore = 0;
+    let humanSelection;
+    let computerSelection;
+
+        function playRound(humanChoice, computerChoice){
+        //set variable with parameters for human choice case insensitive
+        let humanChoiceLowercase =  humanChoice.toLowerCase();
+
+        //create variable to store the correct answer
+        let correctAnswer;        
+
+        //check winner
+            if((humanChoiceLowercase === "rock" && computerChoice === "scissors") || humanChoiceLowercase === "scissors" && computerChoice === "rock"){
+                
+                correctAnswer = "rock";
+            }
+            else if((humanChoiceLowercase === "scissors" && computerChoice === "paper") || humanChoiceLowercase === "paper" && computerChoice === "scissors"){
+            //If paper and scissors set correct answer scissors
+                    
+                    correctAnswer = "scissors";
+                }
+            else if((humanChoiceLowercase === "rock" && computerChoice === "paper") || humanChoiceLowercase === "paper" && computerChoice === "rock"){
+                //If rock and paper set correct answer paper
+                    
+                    correctAnswer = "paper";
+                }
+            else{
+                //If both inputs are the same then return with tie
+                console.log("TIE ROUND");    
+                return;
+                }
+
+
+            if (correctAnswer === humanChoiceLowercase){
+                //If correct answer is User return winner user 
+                ++humanScore;
+                console.log(`You Win!, ${humanChoiceLowercase} beats ${computerChoice}`);
+            } else{
+                //If If correct answer is computer return winner computer
+                ++computerScore;
+                console.log(`You Lose!, ${computerChoice} beats ${humanChoiceLowercase}`);
+            }
+            console.log(`-----ROUND END-----`);
+            console.log(`SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
+
+        }
+
+    for (let i = 0; i < 5 ; i++){
+        console.log(`-----NEW ROUND-----`);
+        humanSelection = getHumanChoice();
+        computerSelection = getComputerChoice();
+        
+        console.log("Computer Choice - ", computerSelection);
+        console.log("Human Choice - ", humanSelection);
+        
+        playRound(humanSelection,computerSelection)
+    } 
+    console.log(`-----GAME OVER-----`);
+    console.log(`FINAL SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
+
+    if(humanScore > computerScore){
+        console.log(`-----YOU WIN GAME-----`);    
+    }else if (humanScore === computerScore){
+        console.log(`-----TIE GAME-----`);    
+    } else {
+        console.log(`-----YOU LOSE GAME-----`); 
+    }
+            
+
+
+}
+
+
+playGame()
