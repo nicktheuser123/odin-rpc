b59eca22ebda6c77e5dde569402ca509ca6a8330
nicktheuser123 <49652976+nicktheuser123@users.noreply.github.com>
2025-08-15 08:51:23 +0530

    Changes

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
  
+let buttons = document.querySelectorAll(".options");
+let results = document.querySelector("#results");
 
+buttons.forEach(btn =>{
+    btn.addEventListener("click", ()=> {
+        playRound(btn.textContent);
+    })
+})
+
+function addResult(text){
+    let newResult = document.createElement("p")
+    newResult.textContent = text;
+    // console.log(results)   
+    results.appendChild(newResult);
+}
 
 function getComputerChoice() {
 
@@ -33,9 +47,6 @@ function getHumanChoice(){
 }
 
 
-
-//play game
-function playGame(){
     //variable to keep track of human score
     let humanScore = 0;
 
@@ -47,7 +58,7 @@ function playGame(){
         function playRound(humanChoice, computerChoice){
         //set variable with parameters for human choice case insensitive
         let humanChoiceLowercase =  humanChoice.toLowerCase();
-
+        computerChoice = getComputerChoice();
         //create variable to store the correct answer
         let correctAnswer;        
 
@@ -68,7 +79,7 @@ function playGame(){
                 }
             else{
                 //If both inputs are the same then return with tie
-                console.log("TIE ROUND");    
+                addResult("TIE ROUND");    
                 return;
                 }
 
@@ -76,41 +87,48 @@ function playGame(){
             if (correctAnswer === humanChoiceLowercase){
                 //If correct answer is User return winner user 
                 ++humanScore;
-                console.log(`You Win!, ${humanChoiceLowercase} beats ${computerChoice}`);
+                addResult("textContent", `You Win!, ${humanChoiceLowercase} beats ${computerChoice}`);
             } else{
                 //If If correct answer is computer return winner computer
                 ++computerScore;
-                console.log(`You Lose!, ${computerChoice} beats ${humanChoiceLowercase}`);
+                addResult("textContent",`You Lose!, ${computerChoice} beats ${humanChoiceLowercase}`);
             }
-            console.log(`-----ROUND END-----`);
-            console.log(`SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
+            addResult(`-----ROUND END-----`);
+            addResult(`SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
+
+            if(humanScore === 5 || computerScore === 5){
+        addResult(`-----GAME OVER-----`);
+        addResult(`FINAL SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
+
+        if(humanScore > computerScore){
+            addResult(`-----YOU WIN GAME-----`);    
+        }else if (humanScore === computerScore){
+            addResult(`-----TIE GAME-----`);    
+        } else {
+            addResult(`-----YOU LOSE GAME-----`); 
+        }
+        humanScore = 0
+        computerScore = 0
+        results.innerHTML = "";
+    }
+
 
         }
 
-    for (let i = 0; i < 5 ; i++){
-        console.log(`-----NEW ROUND-----`);
-        humanSelection = getHumanChoice();
-        computerSelection = getComputerChoice();
+    // for (let i = 0; i < 5 ; i++){
+    //     console.log(`-----NEW ROUND-----`);
+    //     humanSelection = getHumanChoice();
+    //     
         
-        console.log("Computer Choice - ", computerSelection);
-        console.log("Human Choice - ", humanSelection);
+    //     console.log("Computer Choice - ", computerSelection);
+    //     console.log("Human Choice - ", humanSelection);
         
-        playRound(humanSelection,computerSelection)
-    } 
-    console.log(`-----GAME OVER-----`);
-    console.log(`FINAL SCORES: YOU = ${humanScore} vs COMPUTER = ${computerScore}  `);
-
-    if(humanScore > computerScore){
-        console.log(`-----YOU WIN GAME-----`);    
-    }else if (humanScore === computerScore){
-        console.log(`-----TIE GAME-----`);    
-    } else {
-        console.log(`-----YOU LOSE GAME-----`); 
-    }
-            
+    //     playRound(humanSelection,computerSelection)
+    // } 
+    
 
 
-}
 
 
-playGame()
+
+// playGame()
